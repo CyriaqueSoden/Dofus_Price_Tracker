e54efd3929757ad08e3e0e97582295b4387c0885
CyriaqueSoden <73218277+CyriaqueSoden@users.noreply.github.com>
2022-11-04 11:09:34 +0100

    m

--- Data.py
+++ Data.py
@@ -1,7 +1,6 @@
 import mysql.connector
 import coordinate as co
 
-count = 0
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
@@ -10,26 +9,32 @@ mydb = mysql.connector.connect(
 )
 mycursor = mydb.cursor()
 
+count = 0
 color = 0
 
 dataKamas = []
-regionScreen = {"1": 336, "10": 400, "100": 465}
 listName = ['galet rutilant', 'galet brasillant',
             'dolomite', 'substrat de bosquet', 'saphir', 'diamant']
 
 noSpaceListName = []
-
+# creation noSpaceListName
 for i in listName:
     noSpaceListName.append(i.replace(" ", "_"))
 
-listCoo = {}
-listRegion = {}
+
 clicks = {"ini": (116, 8), "croixDelete": (666, 255), "barreDeRecherche": (
     538, 248), "openItem": (913, 284), "KamasLoadItem": (1030, 276)}
 
+listCoo = {}
+# creation listCoo
 for i in clicks:
     listCoo[i] = co.Coordinate(clicks[i][0], clicks[i][1])
 
+
+regionScreen = {"1": 336, "10": 400, "100": 465}
+
+listRegion = {}
+# creation listRegion
 for i in regionScreen:
     listRegion[i] = co.Coordinate(1250, regionScreen[i])
 
